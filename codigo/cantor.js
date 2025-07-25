9202ad0e3e7795a1d857cb1e976c8ddef86a02fb
SALAS DRAI <carmen.tarapues@udea.edu.co>
2025-07-25 16:06:35 -0500

    se guarda avance

--- codigo/cantor.js
+++ codigo/cantor.js
@@ -1,5 +1,8 @@
 function Cantor() {
-    this.balotasSacados = new Array(75).fill(false)
+
+    this.balotasSacadas = new Array(75).fill(false);
+    this.ultimaBalota = 0;
+    this.pendientes = 75;
 
     this.tabla = [];
     for (var f = 0; f < 15; f++) {
@@ -7,22 +10,28 @@ function Cantor() {
         for (var c = 0; c < 5; c++) {
             this.tabla[f][c] = c * 15 + f + 1;
         }
-
     }
-    // metodo para iniciar
-    this.inicar = function (){
-        this.balotasSacadas = new Array (75).fill (false);
 
+    //metodo para iniciar
+    this.iniciar = function () {
+        this.balotasSacadas = new Array(75).fill(false);
+        this.pendientes = 75;
     }
+
     //metodo para sacar balotas
-    this.sacarBalotas = function(){
-        let balota = Math.floor(Math.random()*75) + 1; //funcion para que saque las balotas
-        while(this.balotasSacadas[balota-1]) {
-            balota = Math.floor(Math.random()*75) + 1; //funcion para que saque las balotas
-
+    this.sacarBalota = function () {
+        let totalSacadas = this.balotasSacadas.filter(valor => valor == true).length;
+        if (totalSacadas < 75) {
+            let balota = Math.floor(Math.random() * 75) + 1;
+            while (this.balotasSacadas[balota - 1]) {
+                balota = Math.floor(Math.random() * 75) + 1;
+            }
+            this.balotasSacadas[balota - 1] = true;
+            this.ultimaBalota = balota;
+            this.pendientes = 75 - totalSacadas - 1;
         }
-        this.balotasSacadas[balota-1] = true;
-        this.ultimaBalota = balota;
-        
+        else {
+            window.alert("Ya se acabaron las balotas");
+        }
     }
 }
